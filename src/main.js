a6385b9bd90aabcb31fb6f87830bc93334d24272
hryhorijk <k.hryhoriy88@gmail.com>
2025-06-03 22:37:15 +0200

    01

--- src/main.js
+++ src/main.js
@@ -6,6 +6,14 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("btnDum").addEventListener("click", () => calculateDum());
 });
 
+document.querySelector(".header-logo").addEventListener("click", (event) => {
+    event.preventDefault(); // Zabraň přesměrování, pokud je to odkaz
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth" // Plynulý efekt posouvání
+    });
+});
+
 window.showForm = showForm;
 window.calculateByt = calculateByt;
 window.calculateDum = calculateDum;
