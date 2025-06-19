1c6307cc237f1710eaa8aa0dee21d6f41a6fb725
HryhoriyK <k.hryhoriy88@gmail.com>
2025-06-19 21:22:11 +0200

    01

--- src/main.js
+++ src/main.js
@@ -18,6 +18,23 @@ window.showForm = showForm;
 window.calculateByt = calculateByt;
 window.calculateDum = calculateDum;
 
+  const burgerBtn = document.querySelector('[data-menu-open]');
+  
+  const menu = document.querySelector('[data-menu]');
+  
+  const closeBtn = document.querySelector('[data-menu-close]');
+
+  burgerBtn.addEventListener('click', () => {
+    menu.classList.add('is-open');
+    document.body.classList.add('menu-open');
+  });
+  
+  closeBtn.addEventListener('click', () => {
+    menu.classList.remove('is-open');
+    document.body.classList.remove('menu-open');
+  });
+  
+
 
 
 
